1c82304d15ff7a0edaadece936f2af7746e10266
alex_man <alexmnsbs@gmail.com>
2023-05-18 19:41:44 +0300

    add first test

---
+++ Introduction/src/main/java/com/alex/study/junit/introduction/IntroductionApplication.java
@@ -0,0 +1,8 @@
+package com.alex.study.junit.introduction;
+
+public class IntroductionApplication {
+
+    public static void main(String[] args) {
+    }
+
+}
---
+++ Introduction/src/main/java/com/alex/study/junit/introduction/service/dto/User.java
@@ -0,0 +1,4 @@
+package com.alex.study.junit.introduction.service.dto;
+
+public class User {
+}
