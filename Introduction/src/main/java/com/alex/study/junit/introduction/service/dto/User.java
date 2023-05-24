125ccdd3a8171c96a58098900dace49531b34347
alex_man <alexmnsbs@gmail.com>
2023-05-24 10:58:07 +0300

    add Exeption

--- Introduction/src/main/java/com/alex/study/junit/introduction/service/dto/User.java
+++ Introduction/src/main/java/com/alex/study/junit/introduction/service/dto/User.java
@@ -1,4 +1,10 @@
 package com.alex.study.junit.introduction.service.dto;
 
+import lombok.Getter;
+
+@Getter
 public class User {
+    private String userName;
+    private String password;
+
 }
--- Introduction/src/test/java/com/alex/study/junit/introduction/UserServiceTest.java
+++ Introduction/src/test/java/com/alex/study/junit/introduction/UserServiceTest.java
@@ -2,10 +2,13 @@ package com.alex.study.junit.introduction;
 
 import com.alex.study.junit.introduction.service.UserService;
 import com.alex.study.junit.introduction.service.dto.User;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.collection.IsEmptyCollection;
+import org.hamcrest.collection.IsMapContaining;
 import org.junit.jupiter.api.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 //@TestInstance(TestInstance.Lifecycle.PER_METHOD)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -32,10 +35,50 @@ class UserServiceTest {
     }
 
     @Test
+    void trowExeptionIfUsernameOrPasswordIsNull() {
+        try {
+            userService.login(null, "dimmme");
+            fail("login should trown exeption on null UsrName");
+        } catch (IllegalArgumentException ex) {
+            assertTrue(true);
+        }
+    }
+
+    @Test
+    void trowsExeptionIfUsernameOrPasswordIsNull() {
+        assertThrows(IllegalArgumentException.class, ()-> userService.login(null, "dimmme"));
+        assertThrows(IllegalArgumentException.class, ()-> userService.login("dimmme", null));
+        // если так
+        assertAll(
+                () -> assertThrows(IllegalArgumentException.class, ()-> userService.login(null,
+                        "dimmme")),
+                () -> assertThrows(IllegalArgumentException.class, ()-> userService.login("dimmme", null))
+        );
+    }
+
+    @Test
+    void myTrowsExeptionIfUsernameOrPasswordIsNull() {
+        assertAll(
+                () -> {
+                    var exeption = assertThrows(IllegalArgumentException.class,
+                            ()-> userService.login(null,
+                            "dimmme"));
+                    assertThat(exeption.getMessage()).isEqualTo("userName or password is Null");
+                },
+                () -> assertThrows(IllegalArgumentException.class, ()-> userService.login("dimmme", null))
+        );
+
+    }
+
+    @Test
     void userEmptyIfNoUserAdded() {
         System.out.println("userEmptyIfNoUserAdded " + this);
         var users = userService.getAll();
         assertTrue(users.isEmpty(), ()-> "User list should empty");
+        // asserj
+        assertThat(users).hasSize(1);
+        // hamcrest
+        MatcherAssert.assertThat(users, IsEmptyCollection.empty());
     }
 
     @Test
